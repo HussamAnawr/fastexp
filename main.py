1913e000a00bd8ae633de75d082636fe82b96285
Hussam <o.hussam@gmail.com>
2024-09-17 08:17:19 +0300

    (:

--- main.py
+++ main.py
@@ -8,4 +8,17 @@ router = APIRouter(prefix="/api")
 async def index():
     return {"message": "Hello World"}
 
+@router.get("/items/{item_id}")
+async def get_item(item_id: float):
+    return {"item_id": item_id}
+
+@router.get('/users/me')
+async def read_user():
+    return {"user_id": "the current user"}
+
+@router.get('/users/{name}')
+async def read_user_me(name):
+    return {"user_id": f"the current user {name}"}
+
+
 app.include_router(router)
